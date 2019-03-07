bcb8c878e095739743bbb7d5d587c1919501f30a
lsn4nk <lsn4nk1@gmail.com>
2019-03-07 22:58:49 +0200

    second task

--- husband.js
+++ husband.js
@@ -1,7 +1,11 @@
+let count = 0;
+
 class Husband {
     getSalary () {
+        
         const salary = 1000;
         const newSalary = salary - 200;
+        count++;
         return newSalary;
        
     }
@@ -11,14 +15,20 @@ class Husband {
         for(let i = 0; i <= 2; i++) {
             friendsForWife.push(friends[i]);
         }
+        count++;
         return friendsForWife;
     }
     isSober() {
         const sober = true;
         if(sober === true || false) {
-            return "ТРЕЗВЫЙ";
+            count++;
+            return "SOBER";
         }
+    }
+    getCount() {
+        return count;
     }
 }
 
 module.exports.myHusband = new Husband();
+
--- wife.js
+++ wife.js
@@ -3,7 +3,15 @@ const husband = require('./husband');
 const res = husband.myHusband.getSalary();
 const res1 = husband.myHusband.getFriends();
 const res2 = husband.myHusband.isSober();
+//const res3 = husband.myHusband.getCount();
 
-console.log(res);
-console.log(res1);
-console.log(res2);
+//console.log(res);
+//console.log(res1);
+//console.log(res2);
+//console.log(res3);
+
+function showCount() {
+    const res3 = husband.myHusband.getCount();
+    return res3;
+}
+module.exports.showCount = showCount;
